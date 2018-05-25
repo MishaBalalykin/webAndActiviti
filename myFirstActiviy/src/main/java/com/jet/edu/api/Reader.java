ef25ca5cd622625b827193b1058cf93b71417f38
mr.balalykin <miball4@gmail.com>
2018-05-25 13:14:50 +0300

    change ip on localhost

--- myFirstActiviy/src/main/java/com/jet/edu/api/Reader.java
+++ myFirstActiviy/src/main/java/com/jet/edu/api/Reader.java
@@ -7,4 +7,4 @@ import java.util.List;
 public interface Reader {
     List<User> readFromOracleBD();
 }
-/*C:\Users\mr.balalykin>mvn install:install-file -Dfile=C:\Users\mr.balalykin\Downloads\TryAgainRepo\myFirstActiviy\target\myFirstActiviy-1.0-SNAPSHOT.jar -DpomFile=C:\Users\mr.balalykin\Downloads\TryAgainRepo\myFirstActiviy\pom.xml*/
+/*mvn install:install-file -Dfile=C:\Users\mr.balalykin\Downloads\webAndActiviti\myFirstActiviy\target\myFirstActiviy-1.0-SNAPSHOT.jar -DpomFile=C:\Users\mr.balalykin\Downloads\webAndActiviti\myFirstActiviy\pom.xml*/
